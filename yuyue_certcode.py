6434d05e0f1d2bed6953ed91e74fc9309c876421
loyalwong <loyalwong@gmail.com>
2017-06-03 22:28:15 +0800

    a

--- yuyue_certcode.py
+++ yuyue_certcode.py
@@ -4,7 +4,9 @@ import datetime
 from time import sleep
 from random import random
 
-while 1==1:
+itera = 1
+while itera <= 10000:
     urllib.request.urlretrieve("http://yuyue.shdc.org.cn/verifycode.xujie?id=%27+%20Math.random()",
                            datetime.datetime.now().strftime("%Y%m%d%H%M%S")+".jpg")
     sleep(120*random.random())
+    itera = itera+1
