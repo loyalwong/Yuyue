da87e1cd6c5f738a6e6fb173197e4050905cf85f
Joey.Wang <loyalwong@gmail.com>
2017-06-08 12:41:29 +0800

    a

--- yuyue_certcode.py
+++ yuyue_certcode.py
@@ -7,6 +7,9 @@ from random import random
 if __name__ == "__main__":
     i = 0
     while i<=10000:
-        urllib.request.urlretrieve("http://yuyue.shdc.org.cn/verifycode.xujie?id=%27+%20Math.random()",datetime.datetime.now().strftime("%Y%m%d%H%M%S")+".jpg")
+        try:
+            urllib.request.urlretrieve("http://yuyue.shdc.org.cn/verifycode.xujie?id=%27+%20Math.random()",datetime.datetime.now().strftime("%Y%m%d%H%M%S")+".jpg")
+        except Exception:
+            continue
         sleep(120*random())
         i=i+1
