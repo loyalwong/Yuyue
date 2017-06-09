7018b586bdf6d878c10a673c457f7e0557b30141
Joey.Wang <loyalwong@gmail.com>
2017-06-09 16:17:18 +0800

    a

--- yuyue_certcode_collect.py
+++ yuyue_certcode_collect.py
@@ -7,13 +7,10 @@ import random
 if __name__ == "__main__":
     i = 0
     while i<=10000:
-        try:
-            url_image = urllib.request.urlopen("http://yuyue.shdc.org.cn/verifycode.xujie?id=%27+%20Math.random()")
-            filename = "./data/" + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".jpg"
-            file_image = open(filename,'wb')
-            file_image.write(url_image.read())
-            file_image.close()
-        except Exception:
-            print("exception occurs")
+        url_image = urllib.request.urlopen("http://yuyue.shdc.org.cn/verifycode.xujie?id=%27+%20Math.random()")
+        filename = "./data/" + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".jpg"
+        file_image = open(filename,'wb')
+        file_image.write(url_image.read())
+        file_image.close()
         time.sleep(120*random.random())
         i=i+1
