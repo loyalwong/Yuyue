48d64e9f21ae9f3be92d4e4411be92938f6c36e3
loyalwong <loyalwong@gmail.com>
2017-06-11 17:59:15 +0800

    a

--- yuyue_certcode_collect.py
+++ yuyue_certcode_collect.py
@@ -1,16 +1,25 @@
 # coding=utf-8
 import urllib.request
+import exception
 import datetime
 import time
 import random
 
 if __name__ == "__main__":
     i = 0
+    time_sleep = 0
     while i<=10000:
-        url_image = urllib.request.urlopen("http://yuyue.shdc.org.cn/verifycode.xujie?id=%27+%20Math.random()")
+        i=i+1
+        time.sleep(time_sleep+120*random.random())
+        try:
+            url_image = urllib.request.urlopen("http://yuyue.shdc.org.cn/verifycode.xujie?id=%27+%20Math.random()")
+            content_image = url_image.read()
+            time_sleep = 0
+        except Exception:
+            time_sleep = time_sleep + 60*10 + 120*random.random()
+
         filename = "./data/" + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".jpg"
         file_image = open(filename,'wb')
-        file_image.write(url_image.read())
+        file_image.write(content_image)
         file_image.close()
-        time.sleep(120*random.random())
-        i=i+1
+
